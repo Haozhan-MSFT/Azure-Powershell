c25fc25c1420b6d22605ce6de3eeb945013bb3e4
Haozhan-MSFT <141201616+Haozhan-MSFT@users.noreply.github.com>
2023-12-25 14:08:43 +0800

    Adding script to get all user's signin datetime, and update readme accordingly

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 
 Repo for sample PowerShell script related to Microsoft Entra (previously Azure AD).
 
+## Sample Scenarios
+| Sample Name | Description | Module required | Misc. |
+|---|---|---|---|
+| [Get all user's last signin datetime](all-user-last-sign-in.ps1) | Retrieves all users' last signin datetime | [MgGraph Powershell SDK](https://learn.microsoft.com/en-us/powershell/microsoftgraph/overview?view=graph-powershell-1.0) | P1/P2 license required |
+
+
---
+++ all-user-last-sign-in.ps1
@@ -0,0 +1,28 @@
+$scp = @("AuditLog.Read.All", "User.Read.All")
+Connect-MgGraph -Scopes $scp
+$path = "C:\temp" #path to store exported CSV
+
+$date = Get-Date -Format "yyyy-MM-dd HHmmss"
+
+#EXTRA CAUTION: below string need to be quoted
+$url = "https://graph.microsoft.com/beta/users?`$select=displayName,userPrincipalName,id,signInActivity"
+
+
+$output = @()
+do{
+    $sign_in = Invoke-MgGraphRequest -Uri $url -Method Get
+    if($sign_in -ne $null){
+        foreach($record in $sign_in.value){
+            $temp = New-Object PSObject -Property @{
+                id=$record.id
+                displayName=$record.displayName
+                userPrincipalName=$record.userPrincipalName
+                lastSignInDateTime=$record.signInActivity.lastSignInDateTime
+            }
+            $output += $temp
+        }
+        $url = $sign_in.'@odata.nextLink' #MS Graph paging handling
+    }
+}while($url -ne $null) #MS Graph paging handling
+Write-Host Finished!
+
